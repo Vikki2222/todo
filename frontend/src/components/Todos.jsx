d937d1fe41fe0d953112311cd2e3d43412dcf873
Vikki2222 <vigneshp18189@gmail.com>
2025-07-15 22:12:35 +0530

    got the package-lock back and created a Todos component

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,10 @@
+export function Todos(){
+    return <div>
+        <h1>
+            go to the gym
+        </h1>
+        <h2>you need to go to the gym</h2>
+    <button>Mark as completed</button>
+    </div>
+
+}
